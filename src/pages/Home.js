556d156f6e6563bdacbe6147b1eba8b7aee22a6d
Risvan6606 <risvangalfan@gmail.com>
2024-03-02 17:04:33 +0530

    navbar ui done

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../componants/Navbar'
+import Body from '../componants/Body'
+import Contact from '../componants/Contact'
+import Footer from '../componants/Footer'
+
+function Home() {
+  return (
+    <>
+    <Navbar/>
+    <Body/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
