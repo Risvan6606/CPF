6290f4cf248c89973302a9f7b9a5bfc1f969ccf3
Risvan6606 <risvangalfan@gmail.com>
2025-05-20 23:53:16 +0530

    done

--- src/componants/Body.js
+++ src/componants/Body.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Body.css"; // 👈 Import the CSS file
 
 function Body() {
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    fetchDatas();
+  }, []);
+
+  const fetchDatas = async () => {
+    const response = await axios.post("http://localhost:3046/assessment");
+    const leads = response.data.data.data;
+    setDatas(leads);
+  };
+
   return (
-    <div>Body</div>
-  )
+    <div className="body-container">
+      <h2 className="body-heading">Lead Information</h2>
+      <table className="styled-table">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {datas.map((item, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{item.Full_Name}</td>
+              <td>{item.Mobile}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Body
+export default Body;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,9 @@
-import React from 'react'
-import Navbar from '../componants/Navbar'
 import Body from '../componants/Body'
-import Contact from '../componants/Contact'
-import Footer from '../componants/Footer'
 
 function Home() {
   return (
     <>
-    <Navbar/>
     <Body/>
-    <Contact/>
-    <Footer/>
     </>
   )
 }
